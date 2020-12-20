4f3cc7599d14fea01f608cd9398393d8d6529dc4
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-12-20 23:17:44 +0900

    Change double brackets to single bracket

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 
 # .vimrc
-if [[ ! -d $HOME/.vim/undo ]]; then mkdir -p $HOME/.vim/undo; fi
+if [ ! -d $HOME/.vim/undo ]; then mkdir -p $HOME/.vim/undo; fi
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
