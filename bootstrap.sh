bc33b5ce62e7f64631f9540d66f48266005040a1
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-12-26 00:06:46 +0900

    Add .gitignore for edu account

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ DOTFILES_DIR=$HOME/dotfiles
 # .gitconfig
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 
+if [ ! -d $HOME/Codes/Education ]; then mkdir -p $HOME/Codes/Education; fi
+ln -sf $DOTFILES_DIR/.gitconfig_edu $HOME/Codes/Education/.gitconfig
+
 # .vimrc
 if [ ! -d $HOME/.vim/undo ]; then mkdir -p $HOME/.vim/undo; fi
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
