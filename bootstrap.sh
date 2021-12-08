f9aaf321409e965e7d5c8dbb76802b497bc8927a
Junwoo Hwang <zzulu.hwang@gmail.com>
2021-12-08 12:03:57 +0900

    Rename dir Education to Educations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-DOTFILES_DIR=$HOME/dotfiles
+DOTFILES_DIR=$HOME/.dotfiles
 
 # .gitconfig
 ln -snf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 
-if [ ! -d $HOME/Codes/Education ]; then mkdir -p $HOME/Codes/Education; fi
-ln -snf $DOTFILES_DIR/.gitconfig.edu $HOME/Codes/Education/.gitconfig
+if [ ! -d $HOME/Codes/Educations ]; then mkdir -p $HOME/Codes/Educations; fi
+ln -snf $DOTFILES_DIR/.gitconfig.edu $HOME/Codes/Educations/.gitconfig
 
 # .vimrc
 if [ ! -d $HOME/.vim ]; then mkdir -p $HOME/.vim; fi
