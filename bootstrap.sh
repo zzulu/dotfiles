2c57f70f99b3cd51b0e365fb7646e0a236f2df77
Junwoo Hwang <zzulu.hwang@gmail.com>
2024-01-17 09:47:20 +0900

    Add: coc.nvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ ln -snf $DOTFILES_DIR/.gitconfig.edu $HOME/Codes/Educations/.gitconfig
 if [ ! -d $HOME/.vim ]; then mkdir -p $HOME/.vim; fi
 if [ ! -d $HOME/.vim/undo ]; then mkdir -p $HOME/.vim/undo; fi
 ln -snf $DOTFILES_DIR/.vim/ftplugin $HOME/.vim/ftplugin
-ln -snf $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -snf $DOTFILES_DIR/.vim/vimrc $HOME/.vim/vimrc
 
 # DefaultkeyBinding.dict (macOS)
 if [[ "$OSTYPE" == "darwin"* ]]; then
