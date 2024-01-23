78b7a3523cbcd41c1e8c7785da0af4e06cf93f43
Junwoo Hwang <zzulu.hwang@gmail.com>
2024-01-23 13:23:08 +0900

    Add: directories and symlink for neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,14 @@ ln -snf $DOTFILES_DIR/.vimrc $HOME/.vimrc
 # Neovim
 if [ ! -d $HOME/.config ]; then mkdir -p $HOME/.config; fi
 if [ ! -d $HOME/.config/nvim ]; then mkdir -p $HOME/.config/nvim; fi
+if [ ! -d $HOME/.config/nvim/lua ]; then mkdir -p $HOME/.config/nvim/lua; fi
+if [ ! -d $HOME/.config/nvim/after ]; then mkdir -p $HOME/.config/nvim/after; fi
+if [ ! -d $HOME/.config/nvim/plugin ]; then mkdir -p $HOME/.config/nvim/plugin; fi
 ln -snf $DOTFILES_DIR/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+
+if which nvim > /dev/null; then
+  ln -snf $(which nvim) $HOME/.local/bin/nv
+fi
 
 # DefaultkeyBinding.dict (macOS)
 if [[ "$OSTYPE" == "darwin"* ]]; then
